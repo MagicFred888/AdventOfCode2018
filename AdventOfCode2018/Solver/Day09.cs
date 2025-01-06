80b8278204b47b740fb3364b6b442a0789235e1f
Fred <6550969+MagicFred888@users.noreply.github.com>
2025-01-06 19:42:36 +0100

    Solver for day 09

---
+++ AdventOfCode2018/Solver/Day09.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode2018.Tools;
+
+namespace AdventOfCode2018.Solver
+{
+    internal partial class Day09 : BaseSolver
+    {
+        public override string PuzzleTitle { get; } = "Marble Mania";
+
+        private (int nbrOfPlayer, int lastMarble) _gameInfo = (0, 0);
+
+        public override string GetSolution1(bool isChallenge)
+        {
+            ExtractData();
+            return RunGame(_gameInfo.nbrOfPlayer, _gameInfo.lastMarble).ToString();
+        }
+
+        public override string GetSolution2(bool isChallenge)
+        {
+            ExtractData();
+            return RunGame(_gameInfo.nbrOfPlayer, 100 * _gameInfo.lastMarble).ToString();
+        }
+
+        private static long RunGame(int nbrOfPlayer, long lastMarble)
+        {
+            int marbleId = 0;
+            long[] players = Enumerable.Repeat((long)0, nbrOfPlayer).ToArray();
+            ChainedNode activeMarble = new(0);
+            do
+            {
+                marbleId++;
+                activeMarble = activeMarble.Next;
+                if (marbleId % 23 == 0)
+                {
+                    // Skip Marble, add to player score and remove marble
+                    players[marbleId % nbrOfPlayer] += marbleId;
+                    for (int i = 0; i < 7; i++)
+                    {
+                        activeMarble = activeMarble.Previous;
+                    }
+                    players[marbleId % nbrOfPlayer] += activeMarble.Previous.Value;
+                    activeMarble.RemovePreviousNode();
+                }
+                else
+                {
+                    // Place new Marble in the loop
+                    ChainedNode newMarble = new(marbleId);
+                    activeMarble.InsertAfter(newMarble);
+                    activeMarble = newMarble;
+                }
+            } while (marbleId < lastMarble);
+            return players.Max();
+        }
+
+        private void ExtractData()
+        {
+            string[] parts = _puzzleInput[0].Split(' ');
+            _gameInfo = (int.Parse(parts[0]), int.Parse(parts[6]));
+        }
+    }
+}
---
+++ AdventOfCode2018/Tools/ChainedNode.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace AdventOfCode2018.Tools
+{
+    public sealed class ChainedNode
+    {
+        public int Value { get; init; }
+        public ChainedNode Next { get; set; }
+        public ChainedNode Previous { get; set; }
+
+        public ChainedNode(int value)
+        {
+            Value = value;
+            Next = this;
+            Previous = this;
+        }
+
+        public void InsertAfter(ChainedNode newNode)
+        {
+            newNode.Next = Next;
+            Next.Previous = newNode;
+            Next = newNode;
+            newNode.Previous = this;
+        }
+
+        public void RemovePreviousNode()
+        {
+            ChainedNode marble = Previous;
+            Previous = marble.Previous;
+            marble.Previous.Next = this;
+        }
+
+        public void DebugPrint(ChainedNode activeMarble)
+        {
+            // Debugging method to print the current state of the game
+            ChainedNode tmp = this;
+            StringBuilder stringBuilder = new();
+            do
+            {
+                bool isActive = tmp == activeMarble;
+                stringBuilder.Append(isActive ? "[" : "");
+                stringBuilder.Append(tmp.Value);
+                stringBuilder.Append(isActive ? "]" : "");
+                stringBuilder.Append("  ");
+                tmp = tmp.Next;
+            } while (tmp != Previous);
+            Debug.WriteLine(stringBuilder.ToString().Trim());
+        }
+    }
+}
